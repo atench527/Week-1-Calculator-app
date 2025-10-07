fc6a74d6d4e7f6435b71754b91ffe7ae2c481cd7
atench527 <tench-A@ulster.ac.uk>
2025-10-07 13:11:12 +0100

    Implemented program code

--- Calculator app/Program.cs
+++ Calculator app/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("enter your first number then press enter");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = FirstNumber + SecondNumber;
+Console.WriteLine("Your total is {0}", result);
